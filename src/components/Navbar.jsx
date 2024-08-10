56891d26da9a1d54a816bb4095095d0b224f96c7
BharadwajRachakonda <rbharadwaj022@gmail.com>
2024-08-10 11:44:25 +0530

    c

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ function Navbar(props) {
               </svg>
               {/* Icon when menu is open */}
               <svg
-                className={`hidden h-6 w-6 ${isOpen ? "" : "hidden"}`}
+                className={`block h-6 w-6 ${isOpen ? "" : "hidden"}`}
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
@@ -56,7 +56,7 @@ function Navbar(props) {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
             </button>
