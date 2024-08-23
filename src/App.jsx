cafb65b66abfda63924536f83ce4d7d890a20cf9
BharadwajRachakonda <rbharadwaj022@gmail.com>
2024-08-23 20:17:09 +0530

    Browser specifications

--- src/App.jsx
+++ src/App.jsx
@@ -57,42 +57,48 @@ export default function App() {
   };
 
   return (
-    <div className="">
-      <div
-        className={
-          (mode === "light"
-            ? `text-black font-normal`
-            : `text-white font-light`) +
-          ` overflow-x-hidden overflow-y-visible tracking-wider`
-        }
-      >
-        <Navbar />
-        <button
-          className="w-12 h-12 rounded-full bg-white opacity-75 flex items-center transition duration-300 focus:outline-none shadow top-20 right-5 md:right-20 z-50 fixed"
-          onClick={() => {
-            handleClick();
-          }}
+    <>
+      <meta
+        name="theme-color"
+        content={mode === "dark" ? "#14255c" : "#e1e8fd"}
+      />
+      <div className="">
+        <div
+          className={
+            (mode === "light"
+              ? `text-black font-normal`
+              : `text-white font-light`) +
+            ` overflow-x-hidden overflow-y-visible tracking-wider`
+          }
         >
-          <div
-            id="switch-toggle"
-            className={`w-8 h-8 relative rounded-full transition duration-500 transform ${
-              mode === "dark"
-                ? "bg-gray-700 opacity-75 translate-x-full"
-                : "bg-yellow-500 opacity-75 -translate-x-2"
-            } p-1 text-white`}
+          <Navbar />
+          <button
+            className="w-12 h-12 rounded-full bg-white opacity-75 flex items-center transition duration-300 focus:outline-none shadow top-20 right-5 md:right-20 z-50 fixed"
+            onClick={() => {
+              handleClick();
+            }}
           >
-            {mode === "dark" ? darkIcon : lightIcon}
-          </div>
-        </button>
-        <CustomCursor clr={mode} />
-        <BgAnimation clr={mode} />
-        <Dashboard clr={mode} />
-        <About clr={mode} />
-        <Projects clr={mode} />
-        <br />
-        <br />
-        <AchievementsAndContact clr={mode} />
+            <div
+              id="switch-toggle"
+              className={`w-8 h-8 relative rounded-full transition duration-500 transform ${
+                mode === "dark"
+                  ? "bg-gray-700 opacity-75 translate-x-full"
+                  : "bg-yellow-500 opacity-75 -translate-x-2"
+              } p-1 text-white`}
+            >
+              {mode === "dark" ? darkIcon : lightIcon}
+            </div>
+          </button>
+          <CustomCursor clr={mode} />
+          <BgAnimation clr={mode} />
+          <Dashboard clr={mode} />
+          <About clr={mode} />
+          <Projects clr={mode} />
+          <br />
+          <br />
+          <AchievementsAndContact clr={mode} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
