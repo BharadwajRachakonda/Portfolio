e2ec72ca0651d82f43ed08305a188398b0a711d8
Bharadwaj <rbharadwaj022@gmail.com>
2025-02-02 13:04:02 +0530

    chainging border

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,22 +10,23 @@ function Dashboard(props) {
       {/* The Whole Main Page starts Here */}
       {/* Text Goes Here */}
       <div className="flex flex-col gap-y-8 justify-center items-center z-10">
-        <img
-          src={self}
-          alt=""
-          className={
-            `delay-150 h-0 md:visible md:h-36 md:w-36 cursor-pointer myimage hover:scale-110 ease-in-out transition md:border-2 ` +
-            (props.clr === "dark" ? `border-white` : `border-black`)
-          }
-          style={{
-            boxShadow:
-              props.clr === "dark"
-                ? `rgba(255, 255, 255, 0.25) 0px 54px 55px, rgba(255, 255, 255, 0.12) 0px -12px 30px, rgba(255, 255, 255, 0.12) 0px 4px 6px, rgba(255, 255, 255, 0.17) 0px 12px 13px, rgba(255, 255, 255, 0.09) 0px -3px 5px`
-                : `rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px
+        <div className="myimageParent">
+          <img
+            src={self}
+            alt=""
+            className={
+              `delay-150 h-0 md:visible md:h-36 md:w-36 cursor-pointer myimage hover:scale-110 ease-in-out transition md:border-2 ` +
+              (props.clr === "dark" ? `border-white` : `border-transparent`)
+            }
+            style={{
+              boxShadow:
+                props.clr === "dark"
+                  ? `rgba(255, 255, 255, 0.25) 0px 54px 55px, rgba(255, 255, 255, 0.12) 0px -12px 30px, rgba(255, 255, 255, 0.12) 0px 4px 6px, rgba(255, 255, 255, 0.17) 0px 12px 13px, rgba(255, 255, 255, 0.09) 0px -3px 5px`
+                  : `rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px
               `,
-          }}
-        />
-
+            }}
+          />
+        </div>
         <h1
           className={
             `relative text-5xl max-[768px]:text-3xl bg-clip-text text-transparent bg-gradient-to-r font-serif ` +
