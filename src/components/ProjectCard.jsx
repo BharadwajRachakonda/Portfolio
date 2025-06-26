89a7b84e5d45ad74bf74775220608bc121c03f2c
Bharadwaj <rbharadwaj022@gmail.com>
2025-06-26 20:41:08 +0530

    changing projects section for clear view

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import VideoPlayer from "./VideoPlayer";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 /*
-    
-    The Project canrd takes the props 
-    
+
+    The Project card takes the props
+
     video
     clr
     title
@@ -29,9 +30,12 @@ function ProjectCard(props) {
     github,
     deployed,
   } = props;
+  const ref = useRef(null);
+
+  const isInView = useInView(ref, { once: true, amount: 1.0 });
   return (
     <div
-      className={`rounded-3xl border-2 backdrop-blur-md transition-all duration-300 overflow-hidden ${
+      className={`md:m-6 rounded-3xl border-2 backdrop-blur-md transition-all duration-300 overflow-hidden max-w-7xl  ${
         clr === "dark"
           ? "hover:shadow-[0_0_40px_3px_rgba(203,213,225,0.8)] border-gray-200 backdrop-brightness-200"
           : "hover:shadow-[0_0_40px_3px_rgba(103,232,249,0.8)] border-gray-800 backdrop-contrast-200"
@@ -43,42 +47,27 @@ function ProjectCard(props) {
       }}
     >
       <motion.div
+        ref={ref}
         initial={{ opacity: 0, rotateX: 30 }}
         whileInView={{ opacity: 1, rotateX: 0 }}
-        transition={{ duration: 0.5, delay: props.delay * 0.1 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
       >
         <div
           id="scrolly-div"
-          className={`max-w-sm rounded-3xl overflow-hidden`}
+          className={`md:flex-row md:flex max-w-7xl rounded-3xl overflow-hidden`}
         >
           <div className="p-4 md:p-6">
-            <div
-              className={`rounded-2xl overflow-hidden border-2 ${
-                clr === "dark" ? "border-gray-200" : "border-gray-950"
-              }`}
-            >
+            <div className={`rounded-2xl overflow-hidden`}>
               <VideoPlayer video={video} />
             </div>
           </div>
-          <div className="px-6 py-4">
-            <div className="font-bold text-base md:text-xl mb-2">{title}</div>
-            <p className="text-sm md:text-base">{description}</p>
-            <br />
-            <div className="text-white font-bold flex flex-row justify-between gap-10">
-              <a target="_blank" href={github}>
-                <div
-                  className={`transition-all rounded-xl text-sm md:text-base duration-150 ease-in-out p-2 hover:scale-105 hover:tracking-widest group hover:-translate-y-1 border-2 ${
-                    clr === "dark"
-                      ? "border-gray-200"
-                      : "border-gray-700 text-black"
-                  }`}
-                >
-                  <i className="fa-brands fa-github group-hover:animate-bounce mr-2"></i>
-                  GitHub
-                </div>
-              </a>
-              {deployed && (
-                <a href={deployed} target="_blank">
+          <div className="min-w-80 flex flex-col justify-center rounded-3xl shadow-lg">
+            <div className="px-6 py-4">
+              <div className="font-bold text-base md:text-xl mb-2">{title}</div>
+              <p className="text-sm md:text-base">{description}</p>
+              <br />
+              <div className="text-white font-bold flex flex-row justify-between gap-10">
+                <a target="_blank" href={github}>
                   <div
                     className={`transition-all rounded-xl text-sm md:text-base duration-150 ease-in-out p-2 hover:scale-105 hover:tracking-widest group hover:-translate-y-1 border-2 ${
                       clr === "dark"
@@ -86,40 +75,54 @@ function ProjectCard(props) {
                         : "border-gray-700 text-black"
                     }`}
                   >
-                    Visit Here
+                    <i className="fa-brands fa-github group-hover:animate-bounce mr-2"></i>
+                    GitHub
                   </div>
                 </a>
-              )}
+                {deployed && (
+                  <a href={deployed} target="_blank">
+                    <div
+                      className={`transition-all rounded-xl text-sm md:text-base duration-150 ease-in-out p-2 hover:scale-105 hover:tracking-widest group hover:-translate-y-1 border-2 ${
+                        clr === "dark"
+                          ? "border-gray-200"
+                          : "border-gray-700 text-black"
+                      }`}
+                    >
+                      Visit Here
+                    </div>
+                  </a>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span
-              className={`inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 ${
-                clr === "dark"
-                  ? "bg-gray-200 text-gray-700"
-                  : "bg-gray-700 text-gray-200"
-              }`}
-            >
-              #{skill1}
-            </span>
-            <span
-              className={`inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 ${
-                clr === "dark"
-                  ? "bg-gray-200 text-gray-700"
-                  : "bg-gray-700 text-gray-200"
-              }`}
-            >
-              #{skill2}
-            </span>
-            <span
-              className={`inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 ${
-                clr === "dark"
-                  ? "bg-gray-200 text-gray-700"
-                  : "bg-gray-700 text-gray-200"
-              }`}
-            >
-              #{skill3}
-            </span>
+            <div className="px-6 pt-4 pb-2">
+              <span
+                className={`inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 ${
+                  clr === "dark"
+                    ? "bg-gray-200 text-gray-700"
+                    : "bg-gray-700 text-gray-200"
+                }`}
+              >
+                #{skill1}
+              </span>
+              <span
+                className={`inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 ${
+                  clr === "dark"
+                    ? "bg-gray-200 text-gray-700"
+                    : "bg-gray-700 text-gray-200"
+                }`}
+              >
+                #{skill2}
+              </span>
+              <span
+                className={`inline-block rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 ${
+                  clr === "dark"
+                    ? "bg-gray-200 text-gray-700"
+                    : "bg-gray-700 text-gray-200"
+                }`}
+              >
+                #{skill3}
+              </span>
+            </div>
           </div>
         </div>
       </motion.div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,10 +8,7 @@ import { motion } from "framer-motion";
 function Projects(props) {
   const { clr } = props; // Destructure clr from props
   return (
-    <div
-      id="Projects"
-      className={`z-0 xl:h-screen flex justify-center items-center`}
-    >
+    <div id="Projects" className={`z-0 flex justify-center items-center`}>
       <div className="flex flex-col gap-y-14 justify-center items-center z-0 w-full px-4">
         <br />
         <br />
@@ -28,41 +25,38 @@ function Projects(props) {
             }
           />
         </h1>
-        <div className="flex flex-row gap-8 xl:gap-2 m-8 flex-wrap md:m-0 h-auto md:w-screen justify-around">
-          <ProjectCard
-            video={News}
-            clr={clr}
-            title="NewsAPP"
-            description="A ReactAPP built using BootStrap and NewsAPI to fetch News"
-            skill1="React"
-            skill2="BootStrap"
-            skill3="API"
-            github="https://github.com/BharadwajRachakonda/NewsAPI"
-            delay={0}
-          />
+        <div className="flex flex-col gap-8 xl:gap-2 m-8 flex-wrap md:m-0 md:w-screen justify-center items-center">
           <ProjectCard
             video={ToDo}
             clr={clr}
             title="ToDo on Cloud"
-            description="A MERN Project that stores a Create, manage, and share to-do lists"
+            description="A MERN to-do app with cloud storage via MongoDB Atlas & Mongoose. Features JWT authentication, bcrypt-based encryption, and secure API endpoints tested using Postman."
             skill1="MERN"
             skill2="Tailwind"
             skill3="API"
             github="https://github.com/BharadwajRachakonda/ToDo-on-Cloud"
             deployed="https://bharadwajrachakonda.github.io/todo-frontend/"
-            delay={1}
+          />
+          <ProjectCard
+            video={News}
+            clr={clr}
+            title="NewsAPP"
+            description="A ReactAPP built using BootStrap and NewsAPI to fetch News, Enhanced UX through responsive design (Bootstrap), error boundaries, and loading states during API fetches."
+            skill1="React"
+            skill2="BootStrap"
+            skill3="API"
+            github="https://github.com/BharadwajRachakonda/NewsAPI"
           />
           <ProjectCard
             video={Supragna}
             clr={clr}
             title="Supragna"
-            description="I developed the webpage for SUPRAGNA, the national-level fest of SAHE."
+            description="Built Supragna fest website using HTML, CSS & JS in 2–3 days. Used templates & AI tools to convert organizers’ sketches into a responsive, user-friendly, and visually structured interface."
             skill1="HTML"
             skill2="CSS"
             skill3="JS"
             github="https://github.com/BharadwajRachakonda/Supragna"
             deployed="https://bharadwajrachakonda.github.io/Supragna/"
-            delay={2}
           />
         </div>
       </div>
